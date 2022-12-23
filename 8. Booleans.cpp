7f90f6112bc5fbec044b3d9180bc42f8ccdd045f
Sabir Bagwan <106504464+sabirbagwan@users.noreply.github.com>
2022-12-23 09:21:56 +0530

    Create 8. Booleans.cpp
    with if and else

---
+++ 8. Booleans.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main(){
+    
+    // bool red_light {true};
+    bool red_light {true};
+    bool green_light {false};
+
+    if(red_light == true){
+        std::cout << "Stop!" << std::endl;
+    }else{
+        std::cout << "Go Through!" << std::endl;
+    }
+
+    if(green_light){
+        std::cout << "The light is Green" << std::endl;
+    }else{
+        std::cout << "The light is not Green" << std::endl;
+    }
+
+    //size of 
+    std::cout << "Sizeof(bool) : " << sizeof(bool) << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "red_light : " << red_light << std::endl;
+    std::cout << "green_light : " << green_light << std::endl;
+
+    std::cout << std::boolalpha;
+    std::cout << "red_light : " << red_light << std::endl;
+    std::cout << "green_light : " << green_light << std::endl;
+
+ 
+    
+    return 0;
+}
